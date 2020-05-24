e3e2fad9c91077fe612b7b26a35f9daaacd0203f
Christian Preß <Christian.Press@syracos.com>
2020-05-24 18:25:41 +0200

    Task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultSubscription = 'Advanced';
 
+  users: {email: string, subscription: string, password: string}[] = [];
+
+  onSubmit(f: NgForm) {
+    if (f.valid) {
+      this.users.push({
+        email: f.form.value.email,
+        subscription: f.form.value.subscription,
+        password: f.form.value.password
+      });
+    }
+
+    f.resetForm({subscription: this.defaultSubscription});
+  }
 }
